beba8a32a765fdfdbaef77e1d5c5e0d108a7235b
paulawaster <pbukowska16@gmail.com>
2017-12-13 21:41:21 +0100

    zadanie 9.1

---
+++ src/main/java/com/kodilla/good/patterns/challenges/MovieStoreMain.java
@@ -0,0 +1,10 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.List;
+
+public class MovieStoreMain {
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+        System.out.println(movieStore.displayTitlesInOneLine());
+    }
+}
